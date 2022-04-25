69f5859366502893b488ecc751f8a4ce7075617e
goodgqbb <101711628+goodgqbb@users.noreply.github.com>
2022-04-25 12:18:20 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -46,33 +46,50 @@ def duixiangcunchu(courseid):
 
 @app.route('/', methods=['POST'])
 def upload():
-#     font = {
-#     'family' : 'SimHei'
-#     }
-#     matplotlib.rc('font', **font)
 
-    # 指定默认字体
-    plt.rcParams['font.sans-serif'] = ['SimHei']
-    # 解决负号'-'显示为方块的问题
-    plt.rcParams['axes.unicode_minus'] = False
     courseid = request.json.get('courseid')
     file = request.json.get('comdata')
     json_data = file
-    plt.figure(figsize=(10,10))#将画布设定为正方形，则绘制的饼图是正圆
-    fig1 = plt.subplot(121)
-    label=['Totally understand','So-so','Not good',"don't understand"]#定义饼图的标签，标签是列表
-    explode=[0.01, 0.01, 0.01, 0.01]#设定各项距离圆心n个半径
+    
+    fig = Figure(dpi = 100)
+    label=['完全明白', '一般般', '不大好', '完全听不懂']
     values=[json_data['完全明白'], json_data['一般般'], json_data['不大好'], json_data['完全听不懂']]
-    plt.pie(values,explode=explode,labels=label,autopct='%1.1f%%')#绘制饼图
-    plt.title('Student voting in this course 1')
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    pie = ax.pie(values,labels=label,autopct='%1.1f%%')
 
-    fig2 = plt.subplot(122)
-    label=['bad', 'good']#定义饼图的标签，标签是列表
-    explode=[0.01, 0.01]#设定各项距离圆心n个半径
+    #图形中的文字无法通过rcParams设置
+    for font in pie[1]:
+        font.set_fontproperties(mpl.font_manager.FontProperties(
+            fname='/app/wxcloudrun/华文楷体.ttf'))
+    path = r'E:\竞赛数据集\设计大赛\1.jpg'
+    fig.savefig(path)
+
+    fig = Figure(dpi = 100)
+    label=['踩', '赞']
     values=[json_data['踩'], json_data['赞']]
-    plt.pie(values,explode=explode,labels=label,autopct='%3.1f%%')#绘制饼图
-    plt.title('Student voting in this course 2')
-    plt.savefig('/app/wxcloudrun/visualization.jpg')
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    pie = ax.pie(values,labels=label,autopct='%1.1f%%')
+    #图形中的文字无法通过rcParams设置
+    for font in pie[1]:
+        font.set_fontproperties(mpl.font_manager.FontProperties(
+            fname='/app/wxcloudrun/华文楷体.ttf'))
+    
+#     plt.figure(figsize=(10,10))#将画布设定为正方形，则绘制的饼图是正圆
+#     fig1 = plt.subplot(121)
+#     label=['Totally understand','So-so','Not good',"don't understand"]#定义饼图的标签，标签是列表
+#     explode=[0.01, 0.01, 0.01, 0.01]#设定各项距离圆心n个半径
+#     values=[json_data['完全明白'], json_data['一般般'], json_data['不大好'], json_data['完全听不懂']]
+#     plt.pie(values,explode=explode,labels=label,autopct='%1.1f%%')#绘制饼图
+#     plt.title('Student voting in this course 1')
+
+#     fig2 = plt.subplot(122)
+#     label=['bad', 'good']#定义饼图的标签，标签是列表
+#     explode=[0.01, 0.01]#设定各项距离圆心n个半径
+#     values=[json_data['踩'], json_data['赞']]
+#     plt.pie(values,explode=explode,labels=label,autopct='%3.1f%%')#绘制饼图
+#     plt.title('Student voting in this course 2')
+#     plt.savefig('/app/wxcloudrun/visualization.jpg')
+    fig.savefig('/app/wxcloudrun/visualization.jpg')
     a = duixiangcunchu(courseid)
     b = {"re":a}
     c = json.dumps(b)
